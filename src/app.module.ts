c2d835c6c6abf59498109fa35c187d8f6a99a5a7
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-08-12 17:37:06 -0300

    feat: criado módulo user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './common/database/database.module';
 import { TasksModule } from './modules/tasks/tasks.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), 
     DatabaseModule, 
-    TasksModule],
+    TasksModule, UserModule],
   controllers: [],
   providers: [],
 })
