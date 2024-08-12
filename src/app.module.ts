4a72184f44b38c027e83313879014687f2a8b30f
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-08-12 16:35:35 -0300

    feat: criando módulo tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './common/database/database.module';
+import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    DatabaseModule, 
+    TasksModule],
   controllers: [],
   providers: [],
 })
